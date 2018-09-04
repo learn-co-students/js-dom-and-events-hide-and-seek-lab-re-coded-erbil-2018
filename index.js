3a5d7bf96b8324352d0d4d504a20bb1e1f1bd301
Ahmed kamil jemil <akjemil@gmail.com>
2018-09-04 23:25:37 +0000

    Done.

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ function getFirstSelector(selector){
 function nestedTarget(nested,target){
   return document.querySelector('#nested .target');
 }
-function increaseRankBy(n){
-  var num = document.querySelectorAll('.ranked-list');
-  for(let a of num){
-    parseInt(a);
-    a+n;
+function increaseRankBy(n) {
+  let num = document.querySelectorAll('.ranked-list');
+  for(let i = 0; i< num.length; i++){
+    let subNum = num[i].children;
+    for(let q = 0; q< subNum.length; q++){
+      subNum[q].innerHTML = parseInt(subNum[q].innerHTML) + n;
+     }
   }
+}
+ function deepestChild() {
+  let num = document.getElementById('grand-node');
+  let nextnum = num.children[0];
+  while (nextnum) {
+    num = nextnum;
+    nextnum = num.children[0];
+  }
+
+  return num;
 }
